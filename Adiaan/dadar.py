8a79639e9561e6911ebf94377c7126ca0dad2f06
adnankemeryusuf-wq <adnankemeryusuf@gmail.com>
2026-01-16 02:54:07 +0300

    Update dadar.py

--- Adiaan/dadar.py
+++ Adiaan/dadar.py
@@ -158,13 +158,13 @@ if not st.session_state.logged_in:
     with col_mid:
         if os.path.exists(LOGO_PATH):
             st.image(LOGO_PATH, width=100)
-        st.markdown("<h1 style='text-align:center; color: #1b5e20;'>Dadar Land Administration Customer Registration System</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align:center; color: #1b5e20;'>Dadar Land Administration Customer Registration System</h2>", unsafe_allow_html=True)
         with st.form("login_form"):
             st.markdown("####  Login")
             u = st.text_input("Username", placeholder="admin")
             p = st.text_input("Password", type="password", placeholder="••••••••")
             if st.form_submit_button("Seeni"):
-                if u == "admin" and p == "123":
+                if u == "DAD" and p == "2026":
                     st.session_state.logged_in = True
                     st.rerun()
                 else:
@@ -378,6 +378,7 @@ else:
     elif menu == "Ba'i":
         st.session_state.logged_in = False
         st.rerun()
+
 
 
 
