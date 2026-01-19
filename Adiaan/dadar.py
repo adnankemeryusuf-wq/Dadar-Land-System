646def9344d8ed1bd5ceb72a4b81c47c5da66aab
adnankemeryusuf-wq <adnankemeryusuf@gmail.com>
2026-01-19 17:07:26 +0300

    Update dadar.py

--- Adiaan/dadar.py
+++ Adiaan/dadar.py
@@ -21,4 +21,7 @@ st.set_page_config(
 BOT_TOKEN = "8357193631:AAHCuSnXzjZTQaglkmcS0gq-EvqnkIQLDBI"
 CHAT_ID_MANAGER = "7329587700"
 LOGO_PATH = "Adiaan/logo.png"
+OL_NAMES = ['Guyyaa', 'Maqaa_Abbaa_Dhimmaa', 'Araddaa', 'Qaxana', 'Gosa_Tajajjilaa', 'Maqaa_Ogeessa', 'Kafaltii_Taj']
+MONTH_ORDER = ["Fulbaana", "Onkololeessa", "Sadaasa", "Muddee", "Amajjii", "Guraandhala", "Bitootessa", "Eebila", "Caamsaa", "Waxabajjii", "Adooleessa", "Hagayya"]
+MONTH_MAP = {9: "Fulbaana", 10: "Onkololeessa", 11: "Sadaasa", 12: "Muddee", 1: "Amajjii", 2: "G
 
