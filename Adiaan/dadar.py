443ffeeb58bae62a9ab77ade35633c4e62a2ca8a
adnankemeryusuf-wq <adnankemeryusuf@gmail.com>
2026-01-19 17:05:47 +0300

    Update dadar.py

--- Adiaan/dadar.py
+++ Adiaan/dadar.py
@@ -1,5 +1,28 @@
 import streamlit as st
 import pandas as pd
+import os
+import io
+import requests
+from datetime import datetime
+from fpdf import FPDF
+import plotly.express as px
+from ethiopian_date import EthiopianDateConverter
+
+# ================= 1. CONFIGURATION & STYLE =================
+# 1. Jalqaba variable kana qopheessi
+LOGO_PATH = "Adiaan/logo.png"
+
+# 2. Page config irratti variable sana fayyadami (Waraabbii malee)
+st.set_page_config(
+    page_title="Dadar Land Customer Registration System", 
+    page_icon=LOGO_PATH if os.path.exists(LOGO_PATH) else "🏢", 
+    layout="wide"
+)
+BOT_TOKEN = "8357193631:AAHCuSnXzjZTQaglkmcS0gq-EvqnkIQLDBI"
+CHAT_ID_MANAGER = "7329587700"
+LOGO_PATH = "Adiaan/logo.png"
+DATA_FILE = "dadar_final_report.txt"import streamlit as st
+import pandas as pd
 import os, io
 from datetime import datetime
 from fpdf import FPDF
@@ -141,4 +164,5 @@ from datetime import datetime
 from fpdf import FPDF
 from PIL import Image
 from ethiopian_date import EthiopianDateConverter
+
 
