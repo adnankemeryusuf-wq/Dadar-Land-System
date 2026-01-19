2dd4c9640ae3f0030912ca2a5c31f2c3fe57f9f7
adnankemeryusuf-wq <adnankemeryusuf@gmail.com>
2026-01-19 20:45:11 +0300

    Update dadar.py

--- Adiaan/dadar.py
+++ Adiaan/dadar.py
@@ -20,7 +20,7 @@ st.set_page_config(
 # ================= 2. STYLE (CSS) =================
 st.markdown(f"""
     <style>
-    /* Halluu background isa haaraa #f4f7f6 */
+    /* Halluu background isa haaraa #2c3e50 */
     .stApp {{
         background-color: #f4f7f6;
     }}
@@ -147,3 +147,4 @@ else:
         if q:
             res = df[df['Maqaa_Abbaa_Dhimmaa'].str.contains(q, case=False, na=False)]
             st.table(res)
+
