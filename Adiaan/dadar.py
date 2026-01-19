8e2ef4f07cc9f1d5027910271e45e920df1775bb
adnankemeryusuf-wq <adnankemeryusuf@gmail.com>
2026-01-20 00:41:57 +0300

    Update dadar.py

--- Adiaan/dadar.py
+++ Adiaan/dadar.py
@@ -77,9 +77,8 @@ if 'role' not in st.session_state: st.session_state.role = None
 # ================= LOGIN =================
 if not st.session_state.logged_in:
     if os.path.exists(LOGO_PATH):
-        st.image(LOGO_PATH, width=120)
-        st.title("Dadar Land Administration                                                                                                                                          Customer Registration System")
-             
+        st.image(LOGO_PATH, width=100)
+        st.title("Dadar Land Administration Customer Registration System")    
     with st.form("Login"):
         u = st.text_input("Username")
         p = st.text_input("Password", type="password")
@@ -181,6 +180,7 @@ else:
     elif menu=="Ba'i":
         st.session_state.logged_in=False
         st.experimental_rerun()
+
 
 
 
