36d91e942ddcd472336429ab982cc1f446ee0fac
adnankemeryusuf-wq <adnankemeryusuf@gmail.com>
2026-02-01 19:54:47 +0300

    Update dadar.py

--- Adiaan/dadar.py
+++ Adiaan/dadar.py
@@ -83,7 +83,7 @@ else:
     with st.sidebar:
         if os.path.exists(LOGO_PATH):
             st.image(LOGO_PATH, width=90)
-        st.markdown("<h2 style='text-align:center;'>WAJJIRA LAFAA</h2>", unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align:center;'>WAJJIRA LAFAA</h3>", unsafe_allow_html=True)
         st.markdown("---")
         menu = st.radio("FILANNOO", ["📊 Dashboard", "📝 Galmee Tajaajilaa", "📈 Gabaasa Galii", "🚪 Logout"])
 
@@ -193,6 +193,7 @@ else:
     elif menu == "🚪 Logout":
         st.session_state.logged_in = False
         st.rerun()
+
 
 
 
